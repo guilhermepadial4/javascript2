e041fbbb79883fa980ee95667471df5e7a837ef2
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-01-20 10:18:05 -0300

    Update projeto

---
+++ script.js
@@ -0,0 +1,13 @@
+var area=document.getElementById('areas')
+
+function entrar(){
+   var nome = prompt('Digite seu nome:')
+
+    if(nome === '' || nome === null){
+        alert('Ops, algo deu errado')
+        area.innerHTML = "Clique no botão para acessar"
+    }else{
+        area.innerHTML = "Bem vindo " + nome   
+    }
+
+}
