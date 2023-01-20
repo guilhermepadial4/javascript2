2f00a7dcbb67b42ca297cd1da181d926e5ae45b5
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-01-20 14:17:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,17 @@ function entrar(){
         alert('Ops, algo deu errado')
         area.innerHTML = "Clique no botão para acessar"
     }else{
-        area.innerHTML = "Bem vindo " + nome   
+        area.innerHTML = "Bem vindo " + nome + " "
+        let botaoSair = document.createElement("button")
+        botaoSair.innerText = "Sair da conta"
+        botaoSair.onclick=sair
+        
+        area.appendChild(botaoSair)  
     }
 
 }
+
+function sair(){
+    alert("Até mais!")
+    area.innerHTML = "Você saiu!"
+}    
