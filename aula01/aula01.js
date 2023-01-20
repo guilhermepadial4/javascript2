77644cbf1313d66aa688bb1a66abcadde84f4441
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-01-20 08:36:31 -0300

    Reset curso

--- aula01/aula01.js
+++
@@ -1,5 +0,0 @@
-"use strict"
-
-window.alert('Olá meus queridos amigos')
-window.prompt('Qual seu nome?')
-window.confirm('Está gostando de sua experiencia na pagina?')
--- aula02/aula02.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-
-//  let n1 = Number(window.prompt('Digite um número: '))
-//  let n2 = Number(window.prompt('Digite outro número: '))
-//  var res=n1+n2
-//  window.alert(`A soma entre ${n1} e ${n2} é ${res}`)
-
-var nome = prompt('Qual seu nome? ')
-document.write(`Olá ${nome}, seu nome tem ${nome.length} letras`)
