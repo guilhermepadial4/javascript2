dd439443f317f03002bbd25487a5e69468dc0216
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-01-23 15:28:10 -0300

    Update index, script

--- script.js
+++ script.js
@@ -1,22 +1,6 @@
 
-function pedir(){
-   var valor =  prompt("Qual sua opção: ")
+function calcular(event){
+    event.preventDefault();
 
-switch (Number(valor)){
-    case 1:
-        alert("Você escolheu 1 = Suco")
-        break
-    case 2:
-        alert("Você escolehu 2 = Água Gelada")
-        break
-    case 3:
-        alert("Você escolheu 3 = Sorvete")
-        break
-    case 4:
-        alert("Você chamou o garçom")
-        break
-    default:
-        alert("Escolha uma opção de 1 a 4")
-        break
-}
+    alert("TESTE")
 }
