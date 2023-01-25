75df74300c181d5edc98e03b99343b8fe076e3fe
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-01-25 11:32:22 -0300

    Update index, script

--- script.js
+++ script.js
@@ -1,23 +1,6 @@
-// let pessoa = {
-//     nome: "Guilherme",
-//     idade: 19,
-//     altura: 1.74,
-//     cargo: "Programador Front-END"
-// }
+let nome = "Guilherme Padial"
+let idade = 19
 
-// let carro = {
-//     modelo: "Golf 1.0",
-//     ano: 2019,
-//     cor: "Branco",
-//     potencia: "140cv"
-// }
+let mensagem = `Meu nome é ${nome} e eu tenho ${idade}`
 
-// console.log(carro)
-
-let usuarios = [
-    {nome: "Guilherme", cargo: "Desenvolvedor Front-End", status: "Ativo"},
-    {nome: "Pedro", cargo: "Desenvolvedor Back-End", status: "Ativo"},
-    {nome: "Maria", cargo: "Desenvolvedor Full-Stack", status: "Ativo"}
-]
-
-console.log(usuarios)
+console.log(mensagem)
