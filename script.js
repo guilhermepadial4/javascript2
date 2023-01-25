de4446e5b67fe0c4ffbeb9355605836a83b66a65
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-01-25 10:14:23 -0300

    Upadate index, script

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
- function acao(){
-     document.write('Executando... <br/>')
-}
+// let pessoa = {
+//     nome: "Guilherme",
+//     idade: 19,
+//     altura: 1.74,
+//     cargo: "Programador Front-END"
+// }
 
-// var timer = setInterval(()=>{
-//     document.write("Executando..!! <br/>")
-// },1000)
+// let carro = {
+//     modelo: "Golf 1.0",
+//     ano: 2019,
+//     cor: "Branco",
+//     potencia: "140cv"
+// }
 
-setTimeout(()=>{
-    console.log('EXECUTOU O TIMEOUT')
-}, 1000)
+// console.log(carro)
+
+let usuarios = [
+    {nome: "Guilherme", cargo: "Desenvolvedor Front-End", status: "Ativo"},
+    {nome: "Pedro", cargo: "Desenvolvedor Back-End", status: "Ativo"},
+    {nome: "Maria", cargo: "Desenvolvedor Full-Stack", status: "Ativo"}
+]
+
+console.log(usuarios)
