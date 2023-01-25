c4168de01e769395ae74f6bab6208d3adbfbeef1
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-01-25 09:23:48 -0300

    Update index, script

--- script.js
+++ script.js
@@ -1,36 +1,11 @@
-
-var peso
-var altura
-var imc
-var resultado
-
-function calcular(event){
-    event.preventDefault();
-
-    peso = document.getElementById("peso").value
-
-    altura = document.getElementById("altura").value
-
-    imc = peso / (altura * altura);
-
-    resultado = document.getElementById('resultado')
-
-    if (imc < 17){
-        resultado.innerHTML = '<br/> Seu resultado foi: ' + imc.toFixed(2) + '<br/> Cuidado, você está muito abaixo do peso.'
-
-    }else if (imc > 17 && imc <= 18.49){
-        resultado.innerHTML = '<br/> Seu resultado foi: ' + imc.toFixed(2) + '<br/> Cuidado, você está abaixo do peso.'
-
-    }else if (imc >= 18.5 && imc < 24.99){
-        resultado.innerHTML = '<br/> Seu resultado foi: ' + imc.toFixed(2) + '<br/> Você está no peso ideal.'
-
-    }else if (imc > 25 && imc <= 29.99){
-        resultado.innerHTML = '<br/> Seu resultado foi: ' + imc.toFixed(2) + '<br/> Você está acima do peso.'
-
-    }else if (imc >= 30){
-        resultado.innerHTML = '<br/> Seu resultado foi: ' + imc.toFixed(2) + '<br/> Cuidado Obeso.'
-    }
-
-    document.getElementById('peso').value = ''
-    document.getElementById('altura').value = ''
+ function acao(){
+     document.write('Executando... <br/>')
 }
+
+// var timer = setInterval(()=>{
+//     document.write("Executando..!! <br/>")
+// },1000)
+
+setTimeout(()=>{
+    console.log('EXECUTOU O TIMEOUT')
+}, 1000)
