344b22f1a3a385e2141e7a0b788a7c6eda98effa
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-01-26 14:35:46 -0300

    Update index, script

--- script.js
+++ script.js
@@ -15,4 +15,11 @@ let pessoa = {
 // console.log(nomePessoa)
 // console.log(cargo)
 
+let nomes = ['Guilherme', 'Rafael', 'Lorena']
+// let {0: Guilherme, 1: Rafa} = nomes
+// console.log(Rafa)
 
+let [primeiro, segundo, terceiro] = nomes
+console.log(primeiro)
+console.log(segundo)
+console.log(terceiro)
