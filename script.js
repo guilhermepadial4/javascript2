e68cb946b9d2ff7e64b5232cc3ad4bf5dee8e4ef
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-01-30 09:14:09 -0300

    Update script

--- script.js
+++ script.js
@@ -5,12 +5,16 @@
 //     console.log(`PASSANDO: ${item} - Está na posição: ${index}`)
 // })
 
-let numeros = [1, 2, 3]
+let numeros = [1, 2, 3, 10]
 let total = numeros.reduce((acumulador, numeros, indice, original,)=>{
     console.log(`${acumulador} - total até o momento`)
     console.log(`${numeros} - valor atual`)
-    console.log(`${indice} - indice atual`)
-    console.log(`${original} - array original`)
+    //console.log(`${indice} - indice atual`)
+    //console.log(`${original} - array original`)
+    console.log("-----------------------")
 
     return acumulador += numeros
 })
+
+
+console.log("Total do REDUCE: " + total)
